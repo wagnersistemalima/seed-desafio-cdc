7861e2fadfa404fa3bdf288b2a3a194f32a86cb1
wagnersistemalima <wagner.sistemalima@gmail.com>
2024-04-11 14:51:32 -0300

    setup inicial

---
+++ src/main/java/com/sistemalima/casadocodigodeveficiente/CasaDoCodigoDeveficienteApplication.java
@@ -0,0 +1,13 @@
+package com.sistemalima.casadocodigodeveficiente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDoCodigoDeveficienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDoCodigoDeveficienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemalima/casadocodigodeveficiente/CasaDoCodigoDeveficienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemalima.casadocodigodeveficiente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoDeveficienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
